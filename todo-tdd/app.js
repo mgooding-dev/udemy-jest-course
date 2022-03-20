f3dc757b6c347e0cf311aa1bb33664c9a33043ad
Michael Gooding <mgooding.dev@gmail.com>
2022-03-20 14:08:19 -0400

    Jest Tutorial Setup and Some Tests

---
+++ todo-tdd/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const todoRoutes = require("./routes/todo.routes");
+const app = express();
+
+app.use("/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+    res.json("Hello world!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is now running!");
+});
+
+module.exports = app;
